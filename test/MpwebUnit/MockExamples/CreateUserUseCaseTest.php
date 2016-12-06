22553f83ad40580db34912633ed8a64dc3e87169
vicaba <vicaba@me.com>
2016-12-06 12:48:09 +0100

    Added fakeTest demo

--- test/MpwebUnit/MockExamples/CreateUserUseCaseTest.php
+++ test/MpwebUnit/MockExamples/CreateUserUseCaseTest.php
@@ -3,24 +3,43 @@ namespace MpwebUnit\MockExamples;
 
 
 use Mpweb\MockExamples\ADependency;
+use Mpweb\MockExamples\CreateUser;
 use Mpweb\MockExamples\CreateUserUseCase;
 use Mpweb\MockExamples\UserRepository;
+use PHPUnit_Framework_MockObject_MockObject;
 use PHPUnit_Framework_TestCase;
 
 class CreateUserUseCaseTest extends PHPUnit_Framework_TestCase
 {
 
+    /**
+     * @var CreateUserUseCase
+     */
     private $createUserUseCase;
 
+    /**
+     * @var PHPUnit_Framework_MockObject_MockObject
+     */
     private $userRepositoryMock;
 
+    /**
+     * @var PHPUnit_Framework_MockObject_MockObject
+     */
     private $aDependencyMock;
+
+    /**
+     * @var PHPUnit_Framework_MockObject_MockObject
+     */
+    private $createUserMock;
+
+
 
     protected function setUp()
     {
 
         $this->userRepositoryMock = $this->createMock(UserRepository::class);
         $this->aDependencyMock = $this->createMock(ADependency::class);
+        $this->createUserMock = $this->createMock(CreateUser::class);
 
         $this->createUserUseCase = new CreateUserUseCase($this->userRepositoryMock, $this->aDependencyMock);
 
@@ -32,5 +51,13 @@ class CreateUserUseCaseTest extends PHPUnit_Framework_TestCase
         $this->createUserUseCase;
     }
 
+    /** @test */
+    public function fakeTest()
+    {
+        $this->userRepositoryMock->expects($this->once())->method('save');
+        $this->createUserUseCase->execute($this->createUserMock);
+    }
+
+    
 
 }

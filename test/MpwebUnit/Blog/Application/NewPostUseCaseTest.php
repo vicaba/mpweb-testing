c41807e7fea664aac0c4cb7cdd76c94b1317f42b
vicaba <vicaba@me.com>
2017-01-19 16:48:01 +0100

    semantic-sol. Tests semanticos.

--- test/MpwebUnit/Blog/Application/NewPostUseCaseTest.php
+++ test/MpwebUnit/Blog/Application/NewPostUseCaseTest.php
@@ -12,6 +12,10 @@ use Mpweb\Blog\Domain\PostRepository;
 class NewPostUseCaseTest extends \PHPUnit_Framework_TestCase
 {
 
+    const VALID_TITLE = "A Title";
+
+    const VALID_CONTENT = "Some Content";
+
     private $newPostUseCase;
 
     private $postRepository;
@@ -22,7 +26,7 @@ class NewPostUseCaseTest extends \PHPUnit_Framework_TestCase
 
     protected function setUp()
     {
-        $this->post = new Post("aTitle", "someContent");
+        $this->post = new Post(self::VALID_TITLE, self::VALID_CONTENT);
         $this->postRepository = $this->createMock(PostRepository::class);
         $this->eventQueue = $this->createMock(EventQueue::class);
         $this->newPostUseCase = new NewPostUseCase($this->postRepository, $this->eventQueue);
@@ -30,81 +34,124 @@ class NewPostUseCaseTest extends \PHPUnit_Framework_TestCase
 
     protected function tearDown()
     {
+        $this->post = null;
         $this->postRepository = null;
         $this->eventQueue = null;
         $this->newPostUseCase = null;
     }
 
     /** @test */
-    public function whenExecutedAPostIsPersistedJustOneTimeIfItDoesNotExist()
+    public function shouldPersistAPostOneTimeIfItDoesNotExist()
+    {
+        $this->givenAPostRepositoryThatDoesntHaveASpecificPost();
+        $this->thenThePostShouldBeSavedOnce();
+        $this->whenTheNewPostUseCaseIsExecutedWithASpecificPost();
+    }
+
+    /** @test */
+    public function shouldNotPersistAPostIfItAlreadyExists()
+    {
+        $this->givenAPostRepositoryThatHasASpecificPost();
+        $this->thenThePostShouldNotBeSaved();
+        $this->whenTheNewPostUseCaseIsExecutedWithASpecificPost();
+    }
+
+    /** @test */
+    public function shouldPublishANewPostEventByDefaultIfThePostDoesNotExist()
+    {
+        $this->givenAPostRepositoryThatDoesntHaveASpecificPost();
+        $this->thenAnEventShouldBePublished();
+        $this->whenTheNewPostUseCaseIsExecutedWithASpecificPost();
+    }
+
+    /** @test */
+    public function shouldNotPublishANewPostEventIfThePostAlreadyExists()
+    {
+        $this->givenAPostRepositoryThatHasASpecificPost();
+        $this->thenAnEventShouldNotBePublished();
+        $this->whenTheNewPostUseCaseIsExecutedWithASpecificPost();
+    }
+
+    /** @test */
+    public function shouldPublishANewPostEventIfForcedToPublishAndThePostDoesNotExist()
+    {
+        $this->givenAPostRepositoryThatDoesntHaveASpecificPost();
+        $this->thenAnEventShouldBePublished();
+        $this->whenTheNewPostUseCaseIsExecutedAndForcedToPublishWithASpecificPost();
+    }
+
+    /** @test */
+    public function shouldNotPublishANewPosEventIfForcedNotToPublishAndThePostDoesNotExist()
+    {
+        $this->givenAPostRepositoryThatDoesntHaveASpecificPost();
+        $this->thenAnEventShouldNotBePublished();
+        $this->whenTheNewPostUseCaseIsExecutedAndForcedNotToPublishWithASpecificPost();
+    }
+
+    public function shouldNotPublishANewPostEventIfForcedToPublishAndThePostAlreadyExists()
+    {
+        $this->givenAPostRepositoryThatHasASpecificPost();
+        $this->thenAnEventShouldNotBePublished();
+        $this->whenTheNewPostUseCaseIsExecutedAndForcedNotToPublishWithASpecificPost();
+    }
+
+    private function whenTheNewPostUseCaseIsExecutedWithASpecificPost()
+    {
+        $this->newPostUseCase->execute($this->post);
+    }
+
+    private function whenTheNewPostUseCaseIsExecutedAndForcedToPublishWithASpecificPost()
+    {
+        $this->newPostUseCase->execute($this->post, true);
+    }
+
+    private function whenTheNewPostUseCaseIsExecutedAndForcedNotToPublishWithASpecificPost()
+    {
+        $this->newPostUseCase->execute($this->post, false);
+    }
+
+    private function givenAPostRepositoryThatDoesntHaveASpecificPost()
     {
         $this->postRepository
             ->method('exists')
             ->willReturn(false);
-        $this->postRepository
-            ->expects($this->once())
-            ->method('save');
-        $this->newPostUseCase->execute($this->post);
     }
 
-    /** @test */
-    public function whenExecutedAPostIsNotPersistedIfItAlreadyExists()
+    private function thenThePostShouldBeSavedOnce()
+    {
+        $this->postRepository
+            ->expects($this->once())
+            ->method('save')
+            ->with($this->isInstanceOf(Post::class));
+    }
+
+    private function givenAPostRepositoryThatHasASpecificPost()
     {
         $this->postRepository
             ->method('exists')
             ->willReturn(true);
+    }
+
+    private function thenThePostShouldNotBeSaved()
+    {
         $this->postRepository
             ->expects($this->never())
             ->method('save');
-        $this->newPostUseCase->execute($this->post);
     }
 
-    /** @test */
-    public function whenExecutedAnEventIsPublishedByDefaultIfItDoesNotExist()
+    private function thenAnEventShouldBePublished()
     {
-        $this->postRepository
-            ->method('exists')
-            ->willReturn(false);
         $this->eventQueue
             ->expects($this->once())
-            ->method('publish');
-        $this->newPostUseCase->execute($this->post);
-    }
-
-    /** @test */
-    public function whenExecutedAnEventIsNotPublishedIfItAlreadyExists()
-    {
-        $this->postRepository
-            ->method('exists')
-            ->willReturn(true);
-        $this->eventQueue
-            ->expects($this->never())
-            ->method('publish');
-        $this->newPostUseCase->execute($this->post);
-    }
-
-    /** @test */
-    public function whenExecutedAPostEventIsEmittedIfIsAllowedToPublish()
-    {
-        $this->postRepository
-            ->method('exists')
-            ->willReturn(false);
-        $this->eventQueue
             ->method('publish')
             ->with($this->isInstanceOf(PostEvent::class));
-        $this->newPostUseCase->execute($this->post);
     }
 
-    /** @test */
-    public function whenExecutedAnEventIsNotEmittedIfNotAllowed()
+    private function thenAnEventShouldNotBePublished()
     {
-        $this->postRepository
-            ->method('exists')
-            ->willReturn(false);
         $this->eventQueue
             ->expects($this->never())
             ->method('publish');
-        $this->newPostUseCase->execute($this->post, false);
     }
 
 }

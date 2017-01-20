59df3e36104295236156b4505faee872d132e413
vicaba <vicaba@me.com>
2017-01-20 14:42:14 +0100

    Generic Solver. Test

--- test/MpwebUnit/FizzBuzz/Solver/GenericSolverTest.php
+++ test/MpwebUnit/FizzBuzz/Solver/GenericSolverTest.php
@@ -8,35 +8,18 @@ use Mpweb\FizzBuzz\Solver\GenericSolver;
 class GenericSolverTest extends \PHPUnit_Framework_TestCase
 {
 
-    const FIZZ_WORD = "fizz";
-
-    const BUZZ_WORD = "buzz";
-
-    const FIZZ_BUZZ_WORD = "fizzbuzz";
-
-    private $fizzBuzz;
+    private $solver;
 
     private $number;
 
     protected function setUp()
     {
-        $this->fizzBuzz = new GenericSolver();
+        $this->solver = new GenericSolver();
     }
 
     /**
      * @test
-     * @dataProvider numberDivisibleByThreeProvider
-     */
-    public function shouldReturnFizzForNumbersThatCanBeDividedByThree($number)
-    {
-        $this->givenANumber($number);
-        $this->thenItShouldReturnFizz();
-    }
-
-
-    /**
-     * @test
-     * @dataProvider numberNotDivisibleByThreeProvider
+     * @dataProvider numberProvider
      */
     public function shouldReturnTheNumberIfItIsNotDivisibleByThree($number)
     {
@@ -44,51 +27,11 @@ class GenericSolverTest extends \PHPUnit_Framework_TestCase
         $this->thenItShouldReturnTheNumber();
     }
 
-    /**
-     * @test
-     * @dataProvider numberDivisibleByFiveProvider
-     */
-    public function shouldReturnBuzzForNumbersThatCanBeDividedByFive($number)
-    {
-        $this->givenANumber($number);
-        $this->thenItShouldReturnBuzz();
-    }
-
-    /**
-     * @test
-     * @dataProvider numberDivisibleByThreeAndFiveProvider
-     */
-    public function shouldReturnFizzBuzzForNumbersThatCanBeDividedByThreeAndFive($number)
-    {
-        $this->givenANumber($number);
-        $this->thenItShouldReturnFizzBuzz();
-    }
-
-    public function numberDivisibleByThreeProvider()
+    public function numberProvider()
     {
         return [
-            [3], [6], [9], [12], [18], [24], [36], [48], [96], [102]
-        ];
-    }
-
-    public function numberNotDivisibleByThreeProvider()
-    {
-        return [
-            [2], [4], [7], [13], [22], [31], [32], [62], [97], [152]
-        ];
-    }
-
-    public function numberDivisibleByFiveProvider()
-    {
-        return [
-            [5], [10], [20], [35], [40], [50], [55], [80], [110], [160]
-        ];
-    }
-
-    public function numberDivisibleByThreeAndFiveProvider()
-    {
-        return [
-            [15], [30], [45], [60], [75], [90], [105]
+            [3], [6], [9], [12], [18], [24], [36], [48], [96], [102], [2], [4], [7], [13], [22], [31], [32], [62], [97],
+            [152], [5], [10], [20], [35], [40], [50], [55], [80], [110], [160], [15], [30], [45], [60], [75], [90], [105]
         ];
     }
 
@@ -97,28 +40,10 @@ class GenericSolverTest extends \PHPUnit_Framework_TestCase
         $this->number = $number;
     }
 
-    private function thenItShouldReturnFizz()
-    {
-        $result = $this->fizzBuzz->solve($this->number);
-        $this->assertEquals(self::FIZZ_WORD, $result);
-    }
-
     private function thenItShouldReturnTheNumber()
     {
-        $result = $this->fizzBuzz->solve($this->number);
+        $result = $this->solver->solve($this->number);
         $this->assertEquals((string) $this->number, $result);
-    }
-
-    private function thenItShouldReturnBuzz()
-    {
-        $result = $this->fizzBuzz->solve($this->number);
-        $this->assertEquals(self::BUZZ_WORD, $result);
-    }
-
-    private function thenItShouldReturnFizzBuzz()
-    {
-        $result = $this->fizzBuzz->solve($this->number);
-        $this->assertEquals(self::FIZZ_BUZZ_WORD, $result);
     }
 
 }

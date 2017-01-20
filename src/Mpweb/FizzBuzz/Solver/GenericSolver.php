fe51e5fb1ec2cb98a2850c9aecda76eccf61440f
vicaba <vicaba@me.com>
2017-01-20 13:37:54 +0100

    Solver. Sol

--- src/Mpweb/FizzBuzz/Solver/GenericSolver.php
+++ src/Mpweb/FizzBuzz/Solver/GenericSolver.php
@@ -6,9 +6,18 @@ namespace Mpweb\FizzBuzz\Solver;
 class GenericSolver implements Solver
 {
 
+    const FIZZ = "fizz";
+
+    const BUZZ = "buzz";
+
+    const FIZZBUZZ = "fizzbuzz";
+
     public function solve($input)
     {
-        // TODO: Implement solve() method.
+        if ($input % 5 == 0 && $input % 3 == 0) return self::FIZZBUZZ;
+        if ($input % 3 == 0) return self::FIZZ;
+        if ($input % 5 == 0) return self::BUZZ;
+        return (string) $input;
     }
 
     public function setNext(Solver $solver)

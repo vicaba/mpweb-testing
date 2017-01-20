bb272842dc3897d8ebbc253260c3c445b999f94f
vicaba <vicaba@me.com>
2017-01-20 14:44:48 +0100

    Boilerplate code removed from GenericSolver

--- src/Mpweb/FizzBuzz/Solver/GenericSolver.php
+++ src/Mpweb/FizzBuzz/Solver/GenericSolver.php
@@ -6,23 +6,9 @@ namespace Mpweb\FizzBuzz\Solver;
 class GenericSolver extends Solver
 {
 
-    const FIZZ = "fizz";
-
-    const BUZZ = "buzz";
-
-    const FIZZBUZZ = "fizzbuzz";
-
     public function solve($input)
     {
-        if ($input % 5 == 0 && $input % 3 == 0) return self::FIZZBUZZ;
-        if ($input % 3 == 0) return self::FIZZ;
-        if ($input % 5 == 0) return self::BUZZ;
         return (string) $input;
-    }
-
-    public function setNext(Solver $solver)
-    {
-        // TODO: Implement setNext() method.
     }
 
 }

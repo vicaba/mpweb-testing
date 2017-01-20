d3c27a8549955d7963b2b316c3d09141be4f3d70
vicaba <vicaba@me.com>
2017-01-20 14:54:58 +0100

    FizzBuzzSolver. Test

---
+++ test/MpwebUnit/FizzBuzz/Solver/FizzBuzzSolverTest.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace MpwebUnit\FizzBuzz\Solver;
+
+
+class FizzBuzzSolverTest extends \PHPUnit_Framework_TestCase
+{
+
+    const FIZZ_WORD = "fizz";
+
+    const BUZZ_WORD = "buzz";
+
+    const FIZZ_BUZZ_WORD = "fizzbuzz";
+
+    private $fizzBuzz;
+
+    private $number;
+
+    protected function setUp()
+    {
+        $this->fizzBuzz = new FizzBuzzSolver();
+    }
+
+    /**
+     * @test
+     * @dataProvider numberDivisibleByThreeProvider
+     */
+    public function shouldReturnFizzForNumbersThatCanBeDividedByThree($number)
+    {
+        $this->givenANumber($number);
+        $this->thenItShouldReturnFizz();
+    }
+
+
+    /**
+     * @test
+     * @dataProvider numberNotDivisibleByThreeProvider
+     */
+    public function shouldReturnTheNumberIfItIsNotDivisibleByThree($number)
+    {
+        $this->givenANumber($number);
+        $this->thenItShouldReturnTheNumber();
+    }
+
+    /**
+     * @test
+     * @dataProvider numberDivisibleByFiveProvider
+     */
+    public function shouldReturnBuzzForNumbersThatCanBeDividedByFive($number)
+    {
+        $this->givenANumber($number);
+        $this->thenItShouldReturnBuzz();
+    }
+
+    /**
+     * @test
+     * @dataProvider numberDivisibleByThreeAndFiveProvider
+     */
+    public function shouldReturnFizzBuzzForNumbersThatCanBeDividedByThreeAndFive($number)
+    {
+        $this->givenANumber($number);
+        $this->thenItShouldReturnFizzBuzz();
+    }
+
+    public function numberDivisibleByThreeProvider()
+    {
+        return [
+            [3], [6], [9], [12], [18], [24], [36], [48], [96], [102]
+        ];
+    }
+
+    public function numberNotDivisibleByThreeProvider()
+    {
+        return [
+            [2], [4], [7], [13], [22], [31], [32], [62], [97], [152]
+        ];
+    }
+
+    public function numberDivisibleByFiveProvider()
+    {
+        return [
+            [5], [10], [20], [35], [40], [50], [55], [80], [110], [160]
+        ];
+    }
+
+    public function numberDivisibleByThreeAndFiveProvider()
+    {
+        return [
+            [15], [30], [45], [60], [75], [90], [105]
+        ];
+    }
+
+    private function givenANumber($number)
+    {
+        $this->number = $number;
+    }
+
+    private function thenItShouldReturnFizz()
+    {
+        $result = $this->fizzBuzz->solve($this->number);
+        $this->assertEquals(self::FIZZ_WORD, $result);
+    }
+
+    private function thenItShouldReturnTheNumber()
+    {
+        $result = $this->fizzBuzz->solve($this->number);
+        $this->assertEquals((string) $this->number, $result);
+    }
+
+    private function thenItShouldReturnBuzz()
+    {
+        $result = $this->fizzBuzz->solve($this->number);
+        $this->assertEquals(self::BUZZ_WORD, $result);
+    }
+
+    private function thenItShouldReturnFizzBuzz()
+    {
+        $result = $this->fizzBuzz->solve($this->number);
+        $this->assertEquals(self::FIZZ_BUZZ_WORD, $result);
+    }
+
+}

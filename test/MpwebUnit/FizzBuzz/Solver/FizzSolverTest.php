75f0f836c589696a74128d9b7fd1161dffcf85a1
vicaba <vicaba@me.com>
2017-01-20 14:24:47 +0100

    Fizz Solver. Test

---
+++ test/MpwebUnit/FizzBuzz/Solver/FizzSolverTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace MpwebUnit\FizzBuzz\Solver;
+
+
+class FizzSolverTest extends \PHPUnit_Framework_TestCase
+{
+
+
+    const FIZZ_WORD = "fizz";
+
+
+    private $solver;
+
+    private $number;
+
+    protected function setUp()
+    {
+        $this->solver = new FizzSolver();
+    }
+
+    /**
+     * @test
+     * @dataProvider numberDivisibleByThreeProvider
+     */
+    public function shouldReturnFizzForNumbersThatCanBeDividedByThree($number)
+    {
+        $this->givenANumber($number);
+        $this->thenItShouldReturnFizz();
+    }
+
+    public function numberDivisibleByThreeProvider()
+    {
+        return [
+            [3], [6], [9], [12], [18], [24], [36], [48], [96], [102]
+        ];
+    }
+
+    private function givenANumber($number)
+    {
+        $this->number = $number;
+    }
+
+    private function thenItShouldReturnFizz()
+    {
+        $result = $this->solver->solve($this->number);
+        $this->assertEquals(self::FIZZ_WORD, $result);
+    }
+
+}

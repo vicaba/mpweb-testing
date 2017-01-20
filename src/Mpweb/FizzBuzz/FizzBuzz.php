cb05fa44436386560b613c69567d2f744b48499f
vicaba <vicaba@me.com>
2017-01-20 13:18:08 +0100

    shouldReturnFizzBuzz. Sol

--- src/Mpweb/FizzBuzz/FizzBuzz.php
+++ src/Mpweb/FizzBuzz/FizzBuzz.php
@@ -9,8 +9,11 @@ class FizzBuzz
     const FIZZ = "fizz";
 
     const BUZZ = "buzz";
+
+    const FIZZBUZZ = "fizzbuzz";
     
     public function solve($number) {
+        if ($number % 5 == 0 && $number % 3 == 0) return self::FIZZBUZZ;
         if ($number % 3 == 0) return self::FIZZ;
         if ($number % 5 == 0) return self::BUZZ;
         return (string) $number;

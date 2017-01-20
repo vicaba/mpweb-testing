0e432ea72f1cd59e47b3eca9709cf27593ab2462
vicaba <vicaba@me.com>
2017-01-20 14:36:42 +0100

    Buzz Solver. Test

---
+++ test/MpwebUnit/FizzBuzz/Solver/BuzzSolverTest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace MpwebUnit\FizzBuzz\Solver;
+
+
+class BuzzSolverTest extends \PHPUnit_Framework_TestCase
+{
+
+    const BUZZ_WORD = "buzz";
+
+    private $solver;
+
+    private $number;
+
+    protected function setUp()
+    {
+        $this->solver = new BuzzSolver();
+    }
+
+    /**
+     * @test
+     * @dataProvider numberDivisibleByFiveProvider
+     */
+    public function shouldReturnBuzzForNumbersThatCanBeDividedByFive($number)
+    {
+        $this->givenANumber($number);
+        $this->thenItShouldReturnBuzz();
+    }
+
+    public function numberDivisibleByFiveProvider()
+    {
+        return [
+            [5], [10], [20], [35], [40], [50], [55], [80], [110], [160]
+        ];
+    }
+
+    private function givenANumber($number)
+    {
+        $this->number = $number;
+    }
+
+    private function thenItShouldReturnBuzz()
+    {
+        $result = $this->solver->solve($this->number);
+        $this->assertEquals(self::BUZZ_WORD, $result);
+    }
+
+}

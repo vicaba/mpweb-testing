21e6a19b12d6fac174ef1c7e2606448835295250
vicaba <vicaba@me.com>
2017-01-24 12:08:49 +0100

    behat. base.

---
+++ test/MpwebIntegration/Example/Context/ExampleContext.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace MpwebIntegration\Example\Context;
+
+use Behat\Behat\Context\Context;
+use Mpweb\Example\DummyClass;
+use PHPUnit_Framework_Assert;
+
+final class ExampleContext implements Context
+{
+
+}
---
+++ test/MpwebIntegration/FeatureContext.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MpwebIntegration;
+
+use Behat\Behat\Context\Context;
+
+final class FeatureContext implements Context
+{
+}

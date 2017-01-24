ed270ee3ff40b4265dbc0277c53ec719160a2639
vicaba <vicaba@me.com>
2017-01-24 13:17:18 +0100

    behat-test

--- test/MpwebIntegration/Example/Context/ExampleContext.php
+++ test/MpwebIntegration/Example/Context/ExampleContext.php
@@ -7,6 +7,47 @@ use Mpweb\Example\DummyClass;
 use PHPUnit_Framework_Assert;
 
 final class ExampleContext implements Context
-{
 
+{
+    /**
+     * @Given /a value (\b.+\b) that I don't really need/
+     */
+    public function aValueThatIDonTReallyNeed($value)
+    {
+        $this->useless_value = $value;
+    }
+
+    /**
+     * @Given /temporary storing another value (\b.+\b)/
+     */
+    public function temporaryStoringAnotherValue($value)
+    {
+        $this->useful = $value;
+    }
+
+    /**
+     * @When executing the dummy class
+     */
+    public function executingTheDummyClass()
+    {
+        $dummy_class = new DummyClass;
+
+        $this->dummy_class_response = $dummy_class->getTrue();
+    }
+
+    /**
+     * @Then the result should be true
+     */
+    public function theResultShouldBeTrue()
+    {
+        PHPUnit_Framework_Assert::assertTrue($this->dummy_class_response);
+    }
+
+    /**
+     * @Then the temporary value should still be there
+     */
+    public function theTemporaryValueShouldStillBeThere()
+    {
+        PHPUnit_Framework_Assert::assertNotEmpty($this->useful);
+    }
 }
---
+++ test/MpwebIntegration/Example/Feature/Example.feature
@@ -0,0 +1,11 @@
+Feature: something really really stupid...
+  As a sane person
+  I want to make sure that true is true
+  So that the World doesn't implode
+
+  Scenario: doing nasty stuff
+    Given a value x that I don't really need
+    And temporary storing another value y
+    When executing the dummy class
+    Then the result should be true
+    And the temporary value should still be there
